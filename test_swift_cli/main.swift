3b98ae2c01a71f0a22f3e7cb9444606da2e81915
Juande Santander-Vela <j.santander-vela@skatelescope.org>
2015-09-17 15:58:52 +0100

    Initial Commit

---
+++ test_swift_cli/main.swift
@@ -0,0 +1,12 @@
+//
+//  main.swift
+//  test_swift_cli
+//
+//  Created by Santander-Vela, Juande on 17/09/2015.
+//  Copyright © 2015 Santander-Vela, Juande. All rights reserved.
+//
+
+import Foundation
+
+print("Hello, World!")
+
