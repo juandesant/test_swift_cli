57caae31d52289e8af173462f409b6b6bf2c45cb
Juande Santander-Vela <j.santander-vela@skatelescope.org>
2015-09-29 10:19:27 +0100

    First working version of setting flags (as flip flop) and reading integer values.

--- test_swift_cli/main.swift
+++ test_swift_cli/main.swift
@@ -8,5 +8,35 @@
 
 import Foundation
 
-print("Hello, World!")
+var arguments = Process.arguments.generate()
+var flagA = false
+var valB: Int = 0
 
+while let arg = arguments.next() {
+    print("Parsing \(arg)")
+    switch arg {
+        case "-a":
+            flagA = !flagA
+        case "-b":
+            print("Getting value for -b")
+            let valBarg = arguments.next()
+            valB = Int(valBarg!)!
+            print("Setting \(valBarg)")
+        default:
+            continue;
+    }
+}
+
+for pArg in Process.arguments {
+    print(pArg)
+}
+
+if flagA {
+    print("Flag A was set")
+} else {
+    print("Flag A was not set")
+}
+
+if valB != 0 {
+    print("B was set as \(valB)")
+}
